6279adb3b13e92beebc241111a48e46f334cb8bb
Tomer Shemesh <Tomer@Tomers-Mac-Pro.local>
2015-01-06 05:34:57 -0500

    init commit

---
+++ android-tms/src/main/java/com/shemeshapps/android_tms/Parser.java
@@ -0,0 +1,16 @@
+package com.shemeshapps.android_tms;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+/**
+ * Created by Tomer on 1/6/15.
+ */
+public class Parser {
+    String test = "";
+    Document document;
+    public static void test()
+    {
+
+    }
+}
